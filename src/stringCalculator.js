54e27887cc958ebdd44f99e1ec9542563a204dfc
Naveen-Kushalappa <tknaveen4@gmail.com>
2025-03-11 19:04:17 +0530

    Handle custom delimiter in stringCalculator.js

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -5,9 +5,15 @@ const stringCalculator = (input) => {
         return 0;
     }
 
-    const cleanedInput = _.replace(input, /\n/g, '');
-    const inputArray = _.split(cleanedInput, ',');
+    let cleanedInput = _.replace(input, /\n/g, '');
 
+    let delimiter = ',';
+    if (_.startsWith(cleanedInput, '//')){
+        cleanedInput = _.replace(cleanedInput, /^\/\//, '');
+        delimiter = _.head(cleanedInput);
+        cleanedInput = _.join(_.tail(cleanedInput), '');
+    }
+    const inputArray = _.split(cleanedInput, delimiter);
     return _.reduce(inputArray, (sum, value) => {
         return sum + parseInt(value);
     }, 0);
