aba64c2eaf90452977d80cc407f58c574fd71860
Naveen-Kushalappa <tknaveen4@gmail.com>
2025-03-11 15:39:10 +0530

    Add node_modules in .gitignore Add test.js as a test suite to drive TDD. Add package.json and package.lock.json for dependency management.

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+describe('Test suite', function () {
+    describe('Health check', function () {
+        it('Check health of the test suite', function () {
+            assert.equal(true, true);
+        });
+    });
+});
